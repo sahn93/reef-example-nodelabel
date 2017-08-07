ee4ee6599af0042a7c5613772376fb59a8f15e7c
scan3 <glen15181@gmail.com>
2017-08-07 15:35:48 +0900

    first commit

---
+++ src/main/java/reef/example/nodelabel/REEFYarnNodeLabelTest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package reef.example.nodelabel;
+
+import org.apache.reef.client.DriverConfiguration;
+import org.apache.reef.client.DriverLauncher;
+import org.apache.reef.client.LauncherStatus;
+import org.apache.reef.runtime.yarn.client.YarnClientConfiguration;
+import org.apache.reef.tang.Configuration;
+import org.apache.reef.tang.exceptions.BindException;
+import org.apache.reef.tang.exceptions.InjectionException;
+import org.apache.reef.util.EnvironmentUtils;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The Client for REEF node label example on YARN.
+ */
+public class REEFYarnNodeLabelTest {
+
+  private static final Logger LOG = Logger.getLogger(REEFYarnNodeLabelTest.class.getName());
+
+  /**
+   * Number of milliseconds to wait for the job to complete.
+   */
+  private static final int JOB_TIMEOUT = 10000; // 10 sec.
+
+  /**
+   * Empty private constructor to prohibit instantiation
+   */
+  private REEFYarnNodeLabelTest() {
+  }
+
+  /**
+   * @return the configuration of the runtime
+   */
+  private static Configuration getRuntimeConfiguration() {
+    final Configuration runtimeConf = YarnClientConfiguration.CONF.build();
+    return runtimeConf;
+  }
+
+  /**
+   * @return the configuration of the REEFYarnNodeLabelTest driver.
+   */
+  private static Configuration getDriverConfiguration(String nodeLabelExpression) {
+    // TODO: build DriverConfiguration
+    final Configuration driverConf = DriverConfiguration.CONF
+        .set(DriverConfiguration.GLOBAL_LIBRARIES, EnvironmentUtils.getClassLocation(REEFYarnNodeLabelTestDriver.class))
+        .set(DriverConfiguration.DRIVER_IDENTIFIER, "TEST_REEFYarnNodeLabelTest-" + nodeLabelExpression)
+        .set(DriverConfiguration.ON_DRIVER_STARTED, REEFYarnNodeLabelTestDriver.StartHandler.class)
+        .set(DriverConfiguration.ON_EVALUATOR_ALLOCATED, REEFYarnNodeLabelTestDriver.EvaluatorAllocatedHandler.class)
+        .build();
+    return driverConf;
+  }
+
+  /**
+   * Start a REEF job.
+   *
+   * @param args command line parameters.
+   * @throws BindException      configuration error.
+   * @throws InjectionException configuration error.
+   */
+  public static void main(final String[] args) throws BindException, InjectionException {
+    final Configuration runtimeConf = getRuntimeConfiguration();
+
+    if (args.length != 1) {
+      System.out.println("usage: java REEFYarnNodeLabelTestDriver <node_label_expression>");
+    }
+    final Configuration driverConf = getDriverConfiguration(args[0]);
+
+    final LauncherStatus status = DriverLauncher
+        .getLauncher(runtimeConf)
+        .run(driverConf, JOB_TIMEOUT);
+    LOG.log(Level.INFO, "REEF job completed: {0}", status);
+  }
+}
---
+++ src/main/java/reef/example/nodelabel/REEFYarnNodeLabelTestDriver.java
@@ -0,0 +1,132 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package reef.example.nodelabel;
+
+import org.apache.hadoop.yarn.api.records.NodeId;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.reef.driver.evaluator.AllocatedEvaluator;
+import org.apache.reef.driver.evaluator.EvaluatorRequest;
+import org.apache.reef.driver.evaluator.EvaluatorRequestor;
+import org.apache.reef.tang.Configuration;
+import org.apache.reef.tang.annotations.Unit;
+import org.apache.reef.wake.EventHandler;
+import org.apache.reef.wake.time.event.StartTime;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The Driver code for the REEF Yarn node label test Application
+ */
+@Unit
+public class REEFYarnNodeLabelTestDriver {
+  private static final Logger LOG = Logger.getLogger(REEFYarnNodeLabelTestDriver.class.getName());
+
+  private final EvaluatorRequestor evaluatorRequestor;
+
+  private final String nodeLabelExpression;
+
+  /**
+   * Job driver constructor - instantiated via TANG.
+   *
+   * @param requestor evaluator requestor object used to create new evaluator containers.
+   */
+  @Inject
+  public REEFYarnNodeLabelTestDriver(final EvaluatorRequestor requestor) {
+    this.evaluatorRequestor = requestor;
+    this.nodeLabelExpression = "mylabel";
+    LOG.log(Level.FINE, "Instantiated 'REEFYarnNodeLabelTestDriver'");
+  }
+
+  /**
+   * Handles the StartTime event: Request as single Evaluator.
+   */
+  public final class StartHandler implements EventHandler<StartTime> {
+    @Override
+    public void onNext(final StartTime startTime) {
+      final EvaluatorRequest reqToMylabel = EvaluatorRequest.newBuilder()
+          .setNumber(1)
+          .setMemory(500)
+          .setNumberOfCores(1)
+          .setNodeLabelExpression(REEFYarnNodeLabelTestDriver.this.nodeLabelExpression)
+          .build();
+      LOG.log(Level.INFO, "Requested Evaluator with node label: " + REEFYarnNodeLabelTestDriver.this.nodeLabelExpression);
+      REEFYarnNodeLabelTestDriver.this.evaluatorRequestor.submit(reqToMylabel);
+
+      final EvaluatorRequest reqToDefault = EvaluatorRequest.newBuilder()
+          .setNumber(1)
+          .setMemory(500)
+          .setNumberOfCores(1)
+          .build();
+      LOG.log(Level.INFO, "Requested Evaluator with node label: " + REEFYarnNodeLabelTestDriver.this.nodeLabelExpression);
+      REEFYarnNodeLabelTestDriver.this.evaluatorRequestor.submit(reqToDefault);
+    }
+  }
+
+  /**
+   * Handles AllocatedEvaluator: Submit the HelloTask
+   */
+  public final class EvaluatorAllocatedHandler implements EventHandler<AllocatedEvaluator> {
+    @Override
+    public void onNext(final AllocatedEvaluator allocatedEvaluator) {
+      LOG.log(Level.INFO, "Evaluator Allocated: {0}", allocatedEvaluator);
+
+      final String host = allocatedEvaluator.getEvaluatorDescriptor().getNodeDescriptor()
+          .getInetSocketAddress().getHostString();
+      LOG.log(Level.INFO, "Host: " + host);
+
+      final int port = allocatedEvaluator.getEvaluatorDescriptor().getNodeDescriptor()
+          .getInetSocketAddress().getPort();
+      LOG.log(Level.INFO, "Port: " + port);
+
+      final NodeId nodeid = NodeId.newInstance(host, port);
+      LOG.log(Level.INFO, "NodeId: " + nodeid);
+
+      YarnClient client = YarnClient.createYarnClient();
+      client.init(new YarnConfiguration());
+      client.start();
+
+      Set<String> nodelabels = null;
+
+      try {
+        nodelabels = client.getNodeToLabels().get(nodeid);
+      } catch (YarnException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      LOG.log(Level.INFO, "Node Labels on this node: " + nodelabels);
+
+      LOG.log(Level.INFO, "Submitting Dummy REEF task to AllocatedEvaluator: {0}", allocatedEvaluator);
+
+      // TODO: build TaskConfiguration
+      final Configuration taskConfiguration = null;
+
+      allocatedEvaluator.submitTask(taskConfiguration);
+
+      allocatedEvaluator.close();
+    }
+  }
+}
